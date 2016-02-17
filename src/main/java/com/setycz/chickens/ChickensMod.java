a9efdab82a4215570bf94bdbc7ca33965f991407
Jakub Šlesarík <setycz@gmail.com>
2016-02-17 19:41:40 +0100

    first alpha release

--- src/main/java/com/setycz/chickens/ChickensMod.java
+++ src/main/java/com/setycz/chickens/ChickensMod.java
@@ -31,7 +31,7 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod(modid = ChickensMod.MODID, version = ChickensMod.VERSION)
 public class ChickensMod {
     public static final String MODID = "chickens";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1";
     public static final String CHICKEN = "ChickensChicken";
 
     private static final CreativeTabs tab = new ChickensTab("chickens");
