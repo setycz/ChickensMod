2b43d198bcf8de1c6dc3c449c5bb33093b0a5b6c
Jakub Šlesarík <setycz@gmail.com>
2016-02-18 18:43:40 +0100

    version 0.2

--- src/main/java/com/setycz/chickens/ChickensMod.java
+++ src/main/java/com/setycz/chickens/ChickensMod.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @Mod(modid = ChickensMod.MODID, version = ChickensMod.VERSION)
 public class ChickensMod {
     public static final String MODID = "chickens";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     public static final String CHICKEN = "ChickensChicken";
 
     private static final CreativeTabs tab = new ChickensTab("chickens");
