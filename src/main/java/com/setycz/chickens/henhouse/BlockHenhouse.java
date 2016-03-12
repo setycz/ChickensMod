613bceeb70886b286d371387cafb6fb50a3945b0
Jakub Šlesarík <setycz@gmail.com>
2016-03-12 07:16:01 +0100

    henhouse is an orientable block now

--- src/main/java/com/setycz/chickens/henhouse/BlockHenhouse.java
+++ src/main/java/com/setycz/chickens/henhouse/BlockHenhouse.java
@@ -4,6 +4,9 @@ import com.setycz.chickens.ChickensMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -18,8 +21,11 @@ import net.minecraft.world.World;
  * Created by setyc on 01.03.2016.
  */
 public class BlockHenhouse extends Block implements ITileEntityProvider{
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+
     public BlockHenhouse() {
         super(Material.wood);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
 
     @Override
@@ -38,14 +44,45 @@ public class BlockHenhouse extends Block implements ITileEntityProvider{
     }
 
     @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+
+    @Override
     public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+        worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
+
         if(stack.hasDisplayName()){
             TileEntity tileEntity = worldIn.getTileEntity(pos);
             if (tileEntity instanceof TileEntityHenhouse) {
                 ((TileEntityHenhouse)tileEntity).setCustomName(stack.getDisplayName());
             }
         }
+    }
+
+    @Override
+    public IBlockState getStateForEntityRender(IBlockState state) {
+        return getDefaultState().withProperty(FACING, EnumFacing.SOUTH);
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        EnumFacing enumfacing = EnumFacing.getFront(meta);
+        if (enumfacing.getAxis() == EnumFacing.Axis.Y)
+        {
+            enumfacing = EnumFacing.NORTH;
+        }
+        return getDefaultState().withProperty(FACING, enumfacing);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return ((EnumFacing)state.getValue(FACING)).getIndex();
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, FACING);
     }
 
     @Override
