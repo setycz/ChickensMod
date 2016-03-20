b9060b04b8e70c97fff35503ffc8e4c95903d815
Jakub Šlesarík <setycz@gmail.com>
2016-03-20 20:41:48 +0100

    version increased to 1.1.1

--- src/main/java/com/setycz/chickens/ChickensMod.java
+++ src/main/java/com/setycz/chickens/ChickensMod.java
@@ -43,7 +43,7 @@ import java.util.List;
 @Mod(modid = ChickensMod.MODID, version = ChickensMod.VERSION)
 public class ChickensMod {
     public static final String MODID = "chickens";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.1";
     public static final String CHICKEN = "ChickensChicken";
 
     @Mod.Instance(MODID)
