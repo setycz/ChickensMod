2334666f7b81d5fed0eb2aeed84f442b4a268262
Jakub Šlesarík <setycz@gmail.com>
2016-03-26 06:00:41 +0100

    version increased to 1.3.1

--- src/main/java/com/setycz/chickens/ChickensMod.java
+++ src/main/java/com/setycz/chickens/ChickensMod.java
@@ -46,7 +46,7 @@ import java.util.List;
         dependencies = "required-after:Forge@[12.16.0.1805,);")
 public class ChickensMod {
     public static final String MODID = "chickens";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
     public static final String CHICKEN = "ChickensChicken";
 
     @Mod.Instance(MODID)
