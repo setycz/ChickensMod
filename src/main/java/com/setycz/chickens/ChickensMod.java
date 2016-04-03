394decba9ff88e73b147c44d89ceff16251c8b4d
Jakub Šlesarík <setycz@gmail.com>
2016-04-03 10:11:59 +0200

    Forge updated to 1826, JEI to 177, version changed to 2.0.2

--- src/main/java/com/setycz/chickens/ChickensMod.java
+++ src/main/java/com/setycz/chickens/ChickensMod.java
@@ -43,10 +43,10 @@ import java.util.List;
 @Mod(modid = ChickensMod.MODID,
         version = ChickensMod.VERSION,
         acceptedMinecraftVersions = "[1.9]",
-        dependencies = "required-after:Forge@[12.16.0.1821,);")
+        dependencies = "required-after:Forge@[12.16.0.1826,);")
 public class ChickensMod {
     public static final String MODID = "chickens";
-    public static final String VERSION = "2.0.1";
+    public static final String VERSION = "2.0.2";
     public static final String CHICKEN = "ChickensChicken";
 
     @Mod.Instance(MODID)
