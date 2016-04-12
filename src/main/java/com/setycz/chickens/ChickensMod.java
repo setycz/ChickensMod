0ee90eff18d49c5be4500c8db57770585f25964b
Jakub Šlesarík <setycz@gmail.com>
2016-04-12 21:19:08 +0200

    ersion updated 1.9-2.0.3

--- src/main/java/com/setycz/chickens/ChickensMod.java
+++ src/main/java/com/setycz/chickens/ChickensMod.java
@@ -46,7 +46,7 @@ import java.util.List;
         dependencies = "required-after:Forge@[12.16.0.1863,);")
 public class ChickensMod {
     public static final String MODID = "chickens";
-    public static final String VERSION = "2.0.2";
+    public static final String VERSION = "2.0.3";
     public static final String CHICKEN = "ChickensChicken";
 
     @Mod.Instance(MODID)
