efd2230b4b8849231ef32c23c74aacc575ac11d5
Jakub Šlesarík <setycz@gmail.com>
2016-05-22 19:26:15 +0200

    gradle.properties added

--- src/main/java/com/setycz/chickens/ChickensMod.java
+++ src/main/java/com/setycz/chickens/ChickensMod.java
@@ -47,10 +47,10 @@ import java.util.List;
 @Mod(modid = ChickensMod.MODID,
         version = ChickensMod.VERSION,
         acceptedMinecraftVersions = "[1.9]",
-        dependencies = "required-after:Forge@[12.16.0.1863,);")
+        dependencies = "required-after:Forge@[@FORGEVERSION@,);")
 public class ChickensMod {
     public static final String MODID = "chickens";
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "@VERSION@";
     public static final String CHICKEN = "ChickensChicken";
 
     public static final Logger log = LogManager.getLogger(MODID);
